a62844a4ee59ed9a3e9104f86d0b9004e3d9d211
frankshih41 <ssystem1xtw@gmail.com>
2017-06-08 16:46:21 +0800

    CStudent
    description

---
+++ CStudent.java
@@ -0,0 +1,23 @@
+package com.company;
+
+/**
+ * Created by frank on 2017/6/8.
+ */
+public class CStudent {
+
+    private String sex;
+    private String id;
+
+    public void setSex(String s){
+        sex =s;
+    }
+    public void setId(String s){
+        id =s;
+    }
+    public String getSex(){
+        return sex;
+    }
+    public String getId(){
+        return id;
+    }
+}
